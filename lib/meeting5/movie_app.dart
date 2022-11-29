35ec724d4b9957660c906b36d89ad32dcdf9d202
dimadd <dima.dragos0@gmail.com>
2022-11-29 22:23:42 +0200

    Showing the cover image and the title

--- lib/meeting5/movie_app.dart
+++ lib/meeting5/movie_app.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class MovieApp extends StatefulWidget {
   const MovieApp({super.key});
@@ -9,9 +12,36 @@ class MovieApp extends StatefulWidget {
 
 class _MovieAppState extends State<MovieApp> {
   List<Color> colors = <Color>[Colors.red, Colors.blue, Colors.yellow];
-  List<String> movies = <String>['Inception', 'Red Notice', 'Avatar'];
+  final List<String> _titles = <String>[];
+  final List<String> _images = <String>[];
   int currentPage = 1;
   double totalAvailableSpacing = 70;
+  bool isLoading = true;
+
+  void _getMovies() {
+    get(Uri.parse('https://yts.mx/api/v2/list_movies.json')) //
+        .then((Response response) {
+      response.body;
+      final Map<String, dynamic> map = jsonDecode(response.body) as Map<String, dynamic>;
+      final Map<String, dynamic> data = map['data'] as Map<String, dynamic>;
+      final List<dynamic> movies = data['movies'] as List<dynamic>;
+
+      final Iterable<String> items = movies //
+          .cast<Map<dynamic, dynamic>>()
+          .map((Map<dynamic, dynamic> item) => item['title'] as String);
+
+      final Iterable<String> imageItems = movies //
+          .cast<Map<dynamic, dynamic>>()
+          .map((Map<dynamic, dynamic> item) => item['medium_cover_image'] as String);
+
+      setState(() {
+        _titles.addAll(items);
+        _images.addAll(imageItems);
+        isLoading = false;
+      });
+    });
+  }
+
   final PageController _controller = PageController(
     viewportFraction: 0.7,
     initialPage: 1,
@@ -46,57 +76,72 @@ class _MovieAppState extends State<MovieApp> {
           ],
         ),
       ),
-      body: PageView.builder(
-        controller: _controller,
-        itemBuilder: (BuildContext context, int index) {
-          final bool active = index == currentPage;
-          final double innerPadding = active ? 0 : 25;
-          return Column(
-            children: <Widget>[
-              const SizedBox(
-                height: 80,
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: innerPadding,
-                  left: 20,
-                  right: 20,
-                ),
-                child: AnimatedContainer(
-                  duration: const Duration(
-                    milliseconds: 10,
+      body: Builder(
+        builder: (BuildContext context) {
+          if (isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return PageView.builder(
+            controller: _controller,
+            itemBuilder: (BuildContext context, int index) {
+              final bool active = index == currentPage;
+              final double innerPadding = active ? 0 : 30;
+              return Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 80,
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: colors[index],
-                    boxShadow: active
-                        ? <BoxShadow>[
-                            const BoxShadow(
-                              color: Colors.black12,
-                              spreadRadius: 5,
-                            ),
-                          ]
-                        : null,
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: innerPadding,
+                      left: 20,
+                      right: 20,
+                    ),
+                    child: AnimatedContainer(
+                      duration: const Duration(
+                        milliseconds: 100,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        //color: colors[index],
+                        boxShadow: active
+                            ? <BoxShadow>[
+                                const BoxShadow(
+                                  color: Colors.black12,
+                                  spreadRadius: 5,
+                                ),
+                              ]
+                            : null,
+                      ),
+                      height: active ? 400 : 340,
+                      child: Image.network(
+                        _images[index],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                  height: active ? 400 : 350,
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Center(
-                child: Text(
-                  movies[index],
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 26,
+                  const SizedBox(
+                    height: 15,
                   ),
-                ),
-              ),
-            ],
+                  Center(
+                    child: Text(
+                      _titles[index],
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 26,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+            itemCount: _titles.length,
           );
         },
-        itemCount: colors.length,
       ),
     );
   }
@@ -104,6 +149,7 @@ class _MovieAppState extends State<MovieApp> {
   @override
   void initState() {
     super.initState();
+    _getMovies();
     _controller.addListener(() {
       final int pos = _controller.page!.round();
       if (currentPage != pos) {
